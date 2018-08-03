2ad9bc5bd471b289439e0fd798776b4b467b53bf
Alexandre Martinez <alexandre.martinez@benextcompany.com>
2018-08-03 14:20:36 +0200

    Mock relative layers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div class="event note">
-            <span class="label">Event Displayed</span>
-        </div>
-          <div className="aggregate large-note">
-              <span className="label">Graphics Renderer</span>
-          </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="event note" style={{top: "132px", left: "164px"}}>
+                    <span className="label">Event Displayed</span>
+                </div>
+                <div className="aggregate large-note" style={{top: "100px", left: "100px"}}>
+                    <span className="label">Graphics Renderer</span>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
